727b051051124db9c54d9631e3395873eaedd83c
Visao Business Tech <visaobusinesstech@gmail.com>
2026-02-10 20:01:38 -0300

    Fix: Downgrade material-ui-popup-state to v1.9.3 for MUI v4 compatibility and fix Grid2 import

--- frontend/src/pages/Dashboard/index.js
+++ frontend/src/pages/Dashboard/index.js
@@ -56,7 +56,7 @@ import {
   Box,
 } from "@mui/material";
 import { i18n } from "../../translate/i18n";
-import Grid2 from "@mui/material/Unstable_Grid2/Grid2";
+import Grid2 from "@mui/material/Unstable_Grid2";
 import ForbiddenPage from "../../components/ForbiddenPage";
 import { ArrowDownward, ArrowUpward } from "@material-ui/icons";
 
